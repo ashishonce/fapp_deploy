966392cbe850e5136d90e0a2cd67f1429e62c7a6
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-06-09 15:17:33 +0530

    adding webhook logic

--- fappdeploy/httptrigger/GridEventHandler.cs
+++ fappdeploy/httptrigger/GridEventHandler.cs
@@ -31,10 +31,43 @@ using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 
 public static class GridEventHandler{
     [FunctionName("GridEventHandle")]
-    public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequestMessage req, ILogger log, ExecutionContext context)
+    public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req, ILogger log, ExecutionContext context)
     {
         log.LogInformation("C# HTTP trigger function processed a request.");
-      
-        return (ActionResult)new OkObjectResult($"Hello");
+
+        string requestBody = await req.Content.ReadAsAsync<String>();
+        dynamic requestObject = JsonConvert.DeserializeObject(requestBody);
+        var webhook_res = string.Empty;
+        if (requestObject != null){
+            if (requestObject[0].data != null &&  !string.IsNullOrEmpty(requestObject[0].data.validationCode)){
+                var validcode = requestObject[0].data.validationCode;
+                webhook_res= Newtonsoft.Json.JsonConvert.SerializeObject(new Newtonsoft.Json.Linq.JObject {["validationResponse"]= validcode});
+                return (ActionResult)new OkObjectResult($"{webhook_res}");
+            }
+        }
+
+       
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Awesome-Octocat-App");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+
+            var PATTOKEN =  Environment.GetEnvironmentVariable("PATTOKEN", EnvironmentVariableTarget.Process);
+            log.LogInformation($"PATTOKEN: {PATTOKEN}");
+
+            httpClient.DefaultRequestHeaders.Add("Authorization", PATTOKEN);
+
+
+            var client_payload = new Newtonsoft.Json.Linq.JObject { ["unit "] = false, ["integration"] = true, ["github_SHA"] = "7a6fe10d22b5c44be55698f6d123c6480451e18b"};
+
+            var payload = Newtonsoft.Json.JsonConvert.SerializeObject(new Newtonsoft.Json.Linq.JObject { ["event_type"] = "deploy-command", ["client_payload"] = client_payload });
+            
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await httpClient.PostAsync("https://api.github.com/repos/cs1170353/test_mlops/dispatches", content);
+            var resultString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(resultString);
+            return (ActionResult)new OkObjectResult(resultString);
+        }
     }
 }
